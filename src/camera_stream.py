000347439cc7fec8fa62aa47de26eb7675c94cca
AlessandroAlbe <alessandro.albertini01@universitadipavia.it>
2026-02-05 23:31:09 +0100

    .

--- src/camera_stream.py
+++ src/camera_stream.py
@@ -2,6 +2,15 @@ import cv2
 
 from src.detection_people import start_detection
 
+def prova_img(model, device):
+    img = cv2.imread('assets/test_image.jpg')
+    input_size = 640
+    img = cv2.resize(img, (input_size, input_size))
+
+    #img = start_detection(img, model, device, choice='tracking')
+
+    cv2.imwrite('assets/test_image.jpg', img)
+
 def start_stream(model, device):
     # Open the default camera
     cam = cv2.VideoCapture(0)
