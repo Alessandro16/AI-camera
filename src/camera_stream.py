990ab95e38597af4f94b635000ab002e18383a6b
AlessandroAlbe <alessandro.albertini01@universitadipavia.it>
2026-02-05 23:55:31 +0100

    add video result

--- src/camera_stream.py
+++ src/camera_stream.py
@@ -1,15 +1,5 @@
 import cv2
-
 from src.detection_people import start_detection
-
-def prova_img(model, device):
-    img = cv2.imread('assets/test_image.jpg')
-    input_size = 640
-    img = cv2.resize(img, (input_size, input_size))
-
-    #img = start_detection(img, model, device, choice='tracking')
-
-    cv2.imwrite('assets/test_image.jpg', img)
 
 def start_stream(model, device):
     # Open the default camera
